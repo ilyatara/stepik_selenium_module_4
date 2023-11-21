2cae6ae31169099e8b25250de533aedd6fa8fdd6
Ilya Tarasov <qailyatarasov@gmail.com>
2023-11-21 19:26:02 +0600

    Add tests for appearance and disappearance of the success message

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
 import pytest
 
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize(
     'promo',
     ['offer0', 'offer1', 'offer2', 'offer3', 'offer4', 'offer5', 'offer6',
@@ -21,3 +23,35 @@ def test_guest_can_add_product_to_basket(browser, promo):
     # ASSERT
     page.should_have_product_added(page.product_name)
     page.should_have_basket_price(page.product_price)
+
+
+def test_guest_can_not_see_success_message_after_adding_product_to_basket(browser):
+    # ARRANGE
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url)
+    page.open()
+    # ACT
+    page.add_to_basket()
+    # ASSERT
+    assert page.is_not_element_present(*ProductPageLocators.ADDED_PRODUCT_NOTIFICATION)
+
+
+def test_guest_cant_see_success_message(browser):
+    # ARRANGE
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url)
+    # ACT
+    page.open()
+    # ASSERT
+    assert page.is_not_element_present(*ProductPageLocators.ADDED_PRODUCT_NOTIFICATION)
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    # ARRANGE
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url)
+    page.open()
+    # ACT
+    page.add_to_basket()
+    # ASSERT
+    assert page.is_disappeared(*ProductPageLocators.ADDED_PRODUCT_NOTIFICATION)
