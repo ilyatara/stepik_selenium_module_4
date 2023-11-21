0c1cefc754d35cd9b462b74be1a097208845b94e
Ilya Tarasov <qailyatarasov@gmail.com>
2023-11-21 16:47:28 +0600

    Explicit LoginPage initialization in the test body

--- test_main_page.py
+++ test_main_page.py
@@ -6,18 +6,6 @@ def test_guest_can_go_to_login_page(browser):
     url = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(browser, url)
     page.open()
-    page.should_be_login_link()
-
-
-def test_login_page_opens(browser):
-    # ARRANGE
-    url = 'http://selenium1py.pythonanywhere.com/'
-    main_page = MainPage(browser, url)
-    main_page.open()
-    # ACT
-    main_page.go_to_login_page()
-    # ASSERT
-    login_page = LoginPage(browser, url)
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
