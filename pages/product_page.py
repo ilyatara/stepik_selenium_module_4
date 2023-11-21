4b715e1f03a67b1381e45a0889b8e789b2fbaab7
Ilya Tarasov <qailyatarasov@gmail.com>
2023-11-21 18:15:23 +0600

    Add product_name and product_price page properties

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,14 +6,20 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
 
-    def should_have_product_added_notification(self):
+    def should_have_product_added(self, product_name):
         added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         assert added_product_name == product_name,\
-            f'Expected added product name: "{product_name}", got: "{added_product_name}"'
+            f'Expected added product name: "{self.product_name}", got: "{added_product_name}"'
 
-    def should_have_basket_price_notification(self):
+    def should_have_basket_price(self, product_price):
         basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
-        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         assert basket_price == product_price,\
             f'Expected basket price: {product_price}, got: {basket_price}'
+
+    @property
+    def product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    @property
+    def product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
--- test_product_page.py
+++ test_product_page.py
@@ -4,12 +4,12 @@ from .pages.product_page import ProductPage
 def test_guest_can_add_product_to_basket(browser):
     # ARRANGE
     url = 'http://selenium1py.pythonanywhere.com/' \
-          'catalogue/the-shellcoders-handbook_209/?promo=newYear'
+          'catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, url)
     page.open()
     # ACT
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     # ASSERT
-    page.should_have_product_added_notification()
-    page.should_have_basket_price_notification()
+    page.should_have_product_added(page.product_name)
+    page.should_have_basket_price(page.product_price)
