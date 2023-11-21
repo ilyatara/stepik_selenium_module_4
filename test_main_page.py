66131433a0c5a7d0f7a04edcb2c5486359d99c08
Ilya Tarasov <qailyatarasov@gmail.com>
2023-11-21 15:38:20 +0600

    Move login page opening to a function

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
 
-def test_guest_can_go_to_login_page(browser):
-    browser.get('http://selenium1py.pythonanywhere.com/')
+def go_to_login_page(browser):
     login_link = browser.find_element(By.ID, 'login_link')
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/')
+    go_to_login_page(browser)
